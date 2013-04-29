4aa1eec8793a9406a59c407abf6aae8f7352777a
nakamura-to <toshihiro.nakamura@gmail.com>
2013-04-29 18:18:05 +0900

    Release 0.0.0.6

--- Enku/AssemblyInfo.fs
+++ Enku/AssemblyInfo.fs
@@ -20,7 +20,7 @@ open System.Runtime.CompilerServices;
 [<assembly:AssemblyTitle("Enku.dll")>]
 [<assembly:AssemblyCopyright("Copyright 2013, Toshihiro Nakamura")>]
 [<assembly:AssemblyProduct("Enku")>]
-[<assembly:AssemblyVersion("0.0.0.5")>]
+[<assembly:AssemblyVersion("0.0.0.6")>]
 
 [<assembly:InternalsVisibleTo("Enku.Test")>]
 do()
